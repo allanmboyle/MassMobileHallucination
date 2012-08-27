8e2bde93cb98f0a19be4c612920533086edb7436
aidancasey <aidancasey@gmail.com>
2012-08-27 21:21:35 +1000

    BASIC BASIC load testing working - results are pretty damn average using long polling
    Signed-off-by: aidancasey <aidancasey@gmail.com>

--- server.js
+++ server.js
@@ -3,13 +3,12 @@
 // 
 // @simonraikallen 2012
 // 
-
 var express = require('express');
-var server 		= express.createServer(),  
-	io			= require('socket.io').listen(server),
-	playfield 	= require('./lib/playfield.js'),
-	players 	= require('./lib/players.js');
-    controller 	= require('./lib/controller.js');
+var server = express.createServer(),
+    io = require('socket.io').listen(server),
+    playfield = require('./lib/playfield.js'),
+    players = require('./lib/players.js');
+controller = require('./lib/controller.js');
 
 // The currently connected user list
 var playerList = {};
@@ -18,40 +17,58 @@ var playerList = {};
 server.listen(process.env.PORT || 8080);
 console.log("Server listening on port %d", server.address().port);
 
-io.set('log level', 2); // turn logging down
-io.enable('browser client minification');  // send minified client
-io.enable('browser client etag');          // apply etag caching logic based on version number
-io.enable('browser client gzip');          // gzip the file
-io.set('transports', [                     // enable all transports (optional if you want flashsocket)
- 	'websocket',
-  	'flashsocket',
-  	'htmlfile',
-  	'xhr-polling',
-  	'jsonp-polling',
-]);
 
-process.on('uncaughtException', function(err) {
-  console.log('uncaught error' + err);
+var useLongPolling = true;
+
+if (!useLongPolling) {
+    //sockets
+    io.set('log level', 2); // turn logging down
+    io.enable('browser client minification'); // send minified client
+    io.enable('browser client etag'); // apply etag caching logic based on version number
+    io.enable('browser client gzip'); // gzip the file
+    io.set('transports', [ // enable all transports (optional if you want flashsocket)
+        'websocket', 'flashsocket', 'htmlfile', 'xhr-polling', 'jsonp-polling', ]);
+
+} else {
+    //long polling
+    io.set('log level', 2); // turn logging down
+    io.configure(function () {
+        io.set("transports", ["xhr-polling"]);
+        io.set("polling duration", 10);
+    });
+}
+
+
+process.on('uncaughtException', function (err) {
+    console.log('uncaught error' + err);
 });
 
 // serve up static content images, css etc...
-server.use(express.static(__dirname + '/public')); 
-server.use(express.static(__dirname + '/public/images')); 
-server.use(express.static(__dirname + '/public/lib')); 
+server.use(express.static(__dirname + '/public'));
+server.use(express.static(__dirname + '/public/images'));
+server.use(express.static(__dirname + '/public/lib'));
 
 // Give some things nice names
-server.get('/', function (req, res) { res.sendfile(__dirname + '/public/index.html'); });
-server.get('/playfield', function (req, res) { res.sendfile(__dirname + '/public/playfield.html'); });
-server.get('/test', function (req, res) { res.sendfile(__dirname + '/public/scaletest.html'); });
-server.get('/control', function (req, res) { res.sendfile(__dirname + '/public/controller.html'); });
+server.get('/', function (req, res) {
+    res.sendfile(__dirname + '/public/index.html');
+});
+server.get('/playfield', function (req, res) {
+    res.sendfile(__dirname + '/public/playfield.html');
+});
+server.get('/test', function (req, res) {
+    res.sendfile(__dirname + '/public/scaletest.html');
+});
+server.get('/control', function (req, res) {
+    res.sendfile(__dirname + '/public/controller.html');
+});
 
 // set up socket handlers (one per namespace)
 // Should this move to each of the modules???
 // TODO make te full connections list not global!
 connections = {
-	players: 	io.of('/players').on('connection', players.playerhandlers),
-	playfield: 	io.of('/playfield').on('connection', playfield.playfieldhandlers),
-	controller:	io.of('/control').on('connection', controller.sockethandlers)
+    players: io.of('/players').on('connection', players.playerhandlers),
+    playfield: io.of('/playfield').on('connection', playfield.playfieldhandlers),
+    controller: io.of('/control').on('connection', controller.sockethandlers)
 }
 
 // pass the player list to both modules as this is the main server state.
