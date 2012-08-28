ab000c7e7da94306607ba2af503f95433e904e2c
aidancasey <aidancasey@gmail.com>
2012-08-28 21:48:51 +1000

    removing failed  azure deployment attempts
    Signed-off-by: aidancasey <aidancasey@gmail.com>

--- azure deployment/MMHWebSocketsAzure/WorkerRole1/app.js
+++
@@ -1,6 +0,0 @@
-﻿var http = require('http');
-
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('NodeJS.exe running on Windows Azure!');
-}).listen(8080);
