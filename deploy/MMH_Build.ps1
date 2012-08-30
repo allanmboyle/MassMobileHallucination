a65c440f8fc6a39cdb4a54db5dab6df144ac42fb
AIDAN CASEY <aidancasey@gmail.com>
2012-08-30 14:15:21 +1000

    PowerShell to deploy to Azure
    Signed-off-by: AIDAN CASEY <aidancasey@gmail.com>

---
+++ deploy/MMH_Build.ps1
@@ -0,0 +1,38 @@
+﻿
+
+#Import-AzurePublishSettingsFile –PublishSettingsFile "C:/testdeploy/mysettings.publishsettings" 
+
+$serviceName = "MMH123456"
+
+$workingFolder = "C:\test"
+$pathToSource = "C:\Code\MassMobileHellucination"
+
+cd $workingFolder
+
+#Create Service Folder
+New-AzureServiceProject $serviceName
+
+#Add worker Role folder
+Add-AzureNodeWorkerRole
+
+$source = $pathToSource
+
+$dest = $workingFolder + '\' + $serviceName + '\WorkerRole1'
+
+$exclude = @('*.pdb','*.config')
+Get-ChildItem $source -Recurse -Exclude $exclude | Copy-Item -Destination {Join-Path $dest $_.FullName.Substring($source.length)}
+
+$fso = New-Object -ComObject scripting.filesystemobject
+ 
+$fso.DeleteFolder($dest + '\.idea')
+$fso.DeleteFolder($dest + '\azure deployment')
+$fso.DeleteFolder($dest + '\node_modules')
+cd $dest
+
+npm install
+
+Remove-AzureService 
+
+Remove-AzureStorageAccount -StorageAccountName  $serviceName
+Publish-AzureServiceProject –ServiceName $serviceName –Location "North Central US” -Launch
+
